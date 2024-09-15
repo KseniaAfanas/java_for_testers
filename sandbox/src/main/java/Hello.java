28dc7d37b3d33acb1b71fb544b4ed1cb3a8b9bf9
afanaseva <afasowa@gmail.com>
2024-09-15 21:23:39 +0700

    Эксперименты с функциями

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, KSUSHA");
+        System.out.println(2/2);
+        System.out.println("Hello"+"ks");
+        System.out.println("2+2=" +(2+2));
     }
 }
