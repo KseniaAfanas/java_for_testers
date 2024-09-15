f752cfc7a43d0ae6684d96277b0d1cfbcafd38f1
afanaseva <afasowa@gmail.com>
2024-09-15 23:50:01 +0700

    Выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -0,0 +1,23 @@
+package ru.stqa.geometry.figures;
+
+public class Triangle {
+}
+
+--Функция для вычисления квадратного корня
+public static double sqrt(double a)
+Returns the correctly rounded positive square root of a double value. Special cases:
+
+If the argument is NaN or less than zero, then the result is NaN.
+If the argument is positive infinity, then the result is positive infinity.
+If the argument is positive zero or negative zero, then the result is the same as the argument.
+        Otherwise, the result is the double value closest to the true mathematical square root of the argument value.
+        Parameters:
+a - a value.
+Returns:
+the positive square root of a. If the argument is NaN or less than zero, the result is NaN.
+
+--константа пи  для использования пишем: math.PI
+static double	PI
+The double value that is closer than any other to pi, the ratio of the circumference of a circle to its diameter.
+
+
