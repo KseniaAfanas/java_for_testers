55c29261caf58c287b9f83a04247a5ec2710d4a5
afanaseva <afasowa@gmail.com>
2024-09-16 01:55:13 +0700

    Вычисление площади треугольника

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,17 +1,17 @@
 package ru.stqa.geometry.figures;
 
-import static java.lang.Math.PI;
 import static java.lang.Math.sqrt;
 
 public class Triangle {
            public static void printTriangleArea(double a, double b, double c)
         {
-            var text = String.format("Площадь треугольника со сторонами %f и %f и %f = %f",a,b,c, triangleArea(a,b,c));
+            var text = String.format("Площадь треугольника со сторонами %f и %f и %f = %f",a,b,c,triangleArea(a, b, c));
             System.out.println(text);
         }
 
-        private static Double triangleArea(double a, double b, double c) {
-            return sqrt (PI*(PI - a)*(PI - b)*(PI - c));
+    private static Double triangleArea(double a, double b, double c) {
+            double sqrt1 = sqrt((a+b+c)/2 * ((a+b+c)/2 - a) * ((a+b+c)/2 - b) * ((a+b+c)/2 - c));
+            return sqrt1;
         }
 }
 
