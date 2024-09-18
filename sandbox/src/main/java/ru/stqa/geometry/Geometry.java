a551b278c70d8eb3d18c1aa0e9a2007810507bb3
afanaseva <afasowa@gmail.com>
2024-09-18 23:59:51 +0700

    Оптимизация кода по квадрату и

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -8,9 +8,9 @@ import static java.lang.Math.PI;
 
 public class Geometry {
     public static void main(String[] args) {
-        Square.printSquareArea(7.); /*собственные классы Squere, собственные функции printSquareArea(7.) */
-        Square.printSquareArea(1.);
-        Square.printSquareArea(4.);
+        Square.printSquareArea(new Square(7.)); /*собственные классы Squere, собственные функции printSquareArea(7.) */
+        Square.printSquareArea(new Square(5.));
+        Square.printSquareArea(new Square(3.));
 
         Rectangle.printRectangleArea(3.0,5.0);
         Rectangle.printRectangleArea(7.0,9.0);
