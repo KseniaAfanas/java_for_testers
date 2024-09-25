590f27a17529cbadac6b00332de6b1dd6a8bd90e
afanaseva <afasowa@gmail.com>
2024-09-26 00:57:44 +0700

    Двухуровневая архитектура тестового набора

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -1,27 +1,28 @@
+package tests;
+
 import model.GroupData;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 public class GroupCreationTests extends TestBase{
 
 
     @Test
     public void CanCreateGroup() {
-        opensGroupsPage();
-        createGroup(new GroupData("group name", "group header", "group footer"));
+        app.opensGroupsPage();
+        app.createGroup(new GroupData("group name", "group header", "group footer"));
     }
 
     @Test
     public void CanCreateGroupWithEmptyName() {
-        opensGroupsPage();
-         createGroup(new GroupData());
+        app.opensGroupsPage();
+         app.createGroup(new GroupData());
     }
     @Test
     public void CanCreateGroupWithNameOnly() {
-        opensGroupsPage();
+        app.opensGroupsPage();
         var emptyGroup = new GroupData();
         var groupWithName = emptyGroup.WithName("some name");
-        createGroup(groupWithName);
+        app.createGroup(groupWithName);
     }
 }
 
