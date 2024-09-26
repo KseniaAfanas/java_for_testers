89cf60754ea2ee5b31935c7ba2904bc5d784c738
afanaseva <afasowa@gmail.com>
2024-09-27 00:40:44 +0700

    Классы-помощники

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -8,21 +8,16 @@ public class GroupCreationTests extends TestBase{
 
     @Test
     public void CanCreateGroup() {
-        app.opensGroupsPage();
-        app.createGroup(new GroupData("group name", "group header", "group footer"));
+       app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
     }
 
     @Test
     public void CanCreateGroupWithEmptyName() {
-        app.opensGroupsPage();
-         app.createGroup(new GroupData());
+         app.groups().createGroup(new GroupData());
     }
     @Test
     public void CanCreateGroupWithNameOnly() {
-        app.opensGroupsPage();
-        var emptyGroup = new GroupData();
-        var groupWithName = emptyGroup.WithName("some name");
-        app.createGroup(groupWithName);
+        app.groups().createGroup(new GroupData().WithName("some name"));
     }
 }
 
