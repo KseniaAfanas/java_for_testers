7fdde01c076d735be1b98fd8f278f7de0a6d0c18
afanaseva <afasowa@gmail.com>
2024-10-13 01:59:45 +0700

    Цикл со счётчиком: многократное повторение похожих действий

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -8,9 +8,9 @@ public class GroupCreationTests extends TestBase{
 
 
     @Test
-    public void CanCreateGroup() {
+    public void CanCreateGroup() {//создаем одну группу в адресной книге
         int groupCount = app.groups().getCount();//класс помщник для получения количества групп
-       app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+       app.groups().createGroup(new GroupData("group name", "group header", "group footer"));//создание группы
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount+1, newGroupCount);
     }
@@ -22,6 +22,17 @@ public class GroupCreationTests extends TestBase{
     @Test
     public void CanCreateGroupWithNameOnly() {
         app.groups().createGroup(new GroupData().WithName("some name"));
+    }
+
+    @Test
+    public void CanCreateMultipleGroups() {//создаем несколько групп в адресной книге
+        int n=5;
+        int groupCount = app.groups().getCount();//класс помщник для получения количества групп
+for (int i=0; i<n; i++) {
+    app.groups().createGroup(new GroupData(randomString(i*10), "group header", "group footer"));//создание группы. В качестве наименование будет рандомное randomString длины i*10
+}
+                int newGroupCount = app.groups().getCount();//плучаем новое значение
+        Assertions.assertEquals(groupCount+n, newGroupCount);//новое значение должно быть больше на n
     }
 }
 
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -3,6 +3,8 @@ package tests;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.Random;
+
 public class TestBase {//базовый класс для тестов
     protected static ApplicationManager app;
 
@@ -12,7 +14,14 @@ public class TestBase {//базовый класс для тестов
             app = new ApplicationManager();
             app.init(System.getProperty("browser","chrome"));//firefox
         }
-
     }
+public String randomString(int n){//функция которая генерит случайные строки. int n-длина генерируемой строки
+        var rnd = new Random();
+var result = "";//пустая строка
+for (int i=0; i<n; i++){
+    result=result+(char)('a'+rnd.nextInt(26));// число преобразовать в символ и этот вимвол прибавить к строке/ nextInt генерирует целочисленные значения
+}
+    return result;
+}
 
 }
