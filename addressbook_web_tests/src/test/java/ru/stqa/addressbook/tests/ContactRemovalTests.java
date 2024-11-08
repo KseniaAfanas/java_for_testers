4ec4de7ca366128d8f5cc96b378c2c47dca332bf
afanaseva <afasowa@gmail.com>
2024-11-08 12:46:55 +0700

    Добавлен тест по исключению контакта из группы через UI и проверками в БД

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactRemovalTests.java
@@ -1,10 +1,13 @@
 package ru.stqa.addressbook.tests;
 
+import ru.stqa.addressbook.common.CommonFunctions;
 import ru.stqa.addressbook.model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.stqa.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Random;
 
 public class ContactRemovalTests extends TestBase {//удаление контакта
@@ -37,6 +40,42 @@ public class ContactRemovalTests extends TestBase {//удаление конта
         app.contacts().removeAllContacts();
         Assertions.assertEquals(0, app.hbm().getContactCount());//ожидаем 0 сравниваем с результатом выполнения метода по подсчету количества контактов
     }
+
+@Test
+public void CanRemoveContactFromGroup() {//исключить контакт из группы через UI и проверками в БД
+    if (app.hbm().getGroupCount()==0) {//проверяем наличие группы путем подсчета, если количество=0, то группы нет.
+        var group = new GroupData()
+                .WithName(CommonFunctions.randomString(10))
+                .WithHeader(CommonFunctions.randomString(20))
+                .WithFooter(CommonFunctions.randomString(30));
+        app.groups().createGroup(group);//создаём групп через UI
+    }
+    if (app.hbm().getContactCount()==0) { //проверяем в БД, если контакта нет, то
+        var contact = new ContactData()//создаем нового обьекта через UI
+                .WithLastname(CommonFunctions.randomString(10))
+                .WithFirstname(CommonFunctions.randomString(10))
+                .WithFoto(randomFile("src/test/resources/images"));
+        app.contacts().create(contact);//создане контакта через UI
+    }
+    app.contacts().refreshContactPresent();   // перейдём на страницу, на кототой можно выбрать контакт
+    var contact = app.contacts().getList().get(0); // получим список всех контактов через UI и выбираем первый
+    var group = app.groups().getList().get(0); // получим списсок всех групп через UI и выбираем первую
+
+    app.contacts().refreshContactPresent();   // перейдём на страницу, на кототой можно выбрать контакт
+    app.contacts().addContactInGroup(contact, group); // добавление контакта в группу через UI
+
+    app.contacts().refreshContactPresent();
+    var oldContacts = app.hbm().getContactsInGroup(group);//загружаем контакты
+
+    var rnd=new Random();//в старом списке выбрать какой-то обьект, который будет соответствовать удаляемой группе
+    var index = rnd.nextInt(oldContacts.size());//случайным образом выбираем индекс какого-то элемента из списка old контактс
+    app.contacts().refreshContactPresent();   // перейдём на страницу, на кототой можно удалить контакт, без предварительных проверок
+    app.contacts().removeContactInGroup(oldContacts.get(index), group);    // удаляем контакт из группы
+    var newContacts = app.hbm().getContactsInGroup(group);//загружаем контакты после удаления
+    Assertions.assertEquals(newContacts.size()+1,oldContacts.size());//новый списка должен быть на единицу меньше чем размер старого списка
 }
+
+}
+
 
 
