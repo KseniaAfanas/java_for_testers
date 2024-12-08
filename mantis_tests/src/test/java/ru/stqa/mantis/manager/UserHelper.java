c48a2f5ca26db50a8bd8702063a4b6b71f01e400
afanaseva <afasowa@gmail.com>
2024-12-08 21:15:59 +0700

    Лекция 9.2. REST API для управления почтовым сервером James
    исправление замечания:  Вся работа с driver должна прятаться внутри методов хелперов

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/UserHelper.java
@@ -0,0 +1,18 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class UserHelper {
+    public static void checkUser(String url, String user, String password) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get(url);
+        driver.manage().window().setSize(new Dimension(1920, 1040));
+        driver.findElement(By.id ("realname")).sendKeys(user);
+        driver.findElement(By.id ("password")).sendKeys(password);
+        driver.findElement(By.id ("password-confirm")).sendKeys(password);
+        driver.findElement((By.cssSelector("span.bigger-110"))).click();
+    }
+}
--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserCreationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserCreationTests.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.mantis.common.CommonFunctions;
+import ru.stqa.mantis.manager.UserHelper;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -21,7 +22,7 @@ public class UserCreationTests extends TestBase {
         }
 
         @ParameterizedTest
-        @MethodSource("randomUser")//создание пользователя администратором через панель управления пользователями
+        @MethodSource("randomUser")//создание пользователя в Мантис администратором через панель управления пользователями
         void canCreateUser(String user) throws InterruptedException {
                 var email = String.format("%s@localhost",user);
                 var password = "password";
@@ -34,16 +35,37 @@ public class UserCreationTests extends TestBase {
                 var url = CommonFunctions.extractUrl(messages.get(0).content());
 
                 //app.user().finishCreation(url,password);
-                WebDriver driver = new FirefoxDriver();
-                driver.get(url);
-                driver.manage().window().setSize(new Dimension(1920, 1040));
-                driver.findElement(By.id ("realname")).sendKeys(user);
-                driver.findElement(By.id ("password")).sendKeys(password);
-                driver.findElement(By.id ("password-confirm")).sendKeys(password);
-                driver.findElement((By.cssSelector("span.bigger-110"))).click();
+                UserHelper.checkUser(url, user, password);
 
                 app.htpp().login(user, password);
                 Thread.sleep(500);
                 Assertions.assertTrue(app.htpp().isLoggedUserIn(user));
         }
 }
+/*
+       public class UserCreationTests extends TestBase {
+        public static Stream<String> randomUser() {
+        return Stream.of(CommonFunctions.randomString(8));
+        }
+
+        @ParameterizedTest
+        @MethodSource("randomUser")//создание пользователя в Мантис администратором через панель управления пользователями
+        void canCreateUser(String user) throws InterruptedException {
+                var email = String.format("%s@localhost",user);
+                var password = "password";
+                app.jamesApi().addUser(email,password);// jamesApi альтернативный помошник, который работает через удаленный программный интерфейс.
+                // Создает нового пользователя с заданным именем
+                app.jamesCli().login("administrator","root");//проверяем что можно залогинится с "administrator"/"root"
+                app.jamesCli().openPage(email, user, password); //открываем браузер и заполняем форму создания и отправляем (в браузере, создать класс помощник) Письмо уходит
+
+                var messages = app.mail().receive(email, password, Duration.ofSeconds(10));
+                var url = CommonFunctions.extractUrl(messages.get(0).content());
+
+                //app.user().finishCreation(url,password);
+                UserHelper.checkUser(url, user, password);
+
+                app.htpp().login(user, password);
+                Thread.sleep(500);
+                Assertions.assertTrue(app.htpp().isLoggedUserIn(user));
+        }
+ */
